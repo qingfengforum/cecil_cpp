093969b6676d38ecd05b39d72a69a8b914144bec
yuyuan <yuyuan@nebula-link.com>
2020-01-07 10:58:29 +0800

    add printf.cpp

---
+++ debug/printf/printf.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+
+    unsigned char uchar = 23;
+    printf("uchar: %.3hhu\n", uchar); // 023
+
+    unsigned long long ull = 100005;
+    printf("ull: %llu\n", ull); // 100005
+
+    return 0;
+}
